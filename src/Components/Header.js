23b45ac419b9a10aab7289d27f50b8707f9f8483
alkasah <alkasah485@gmail.com>
2022-12-01 01:38:30 -0800

    updation in navigation and header file

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,4 +6,34 @@ import Portfolio from "../Components/Portfolio";
 import Contact from '../Components/Contact';
 import Resume from '../Components/Resume';
 
+export default function Header () {
 
+  const [currentPage , handlePageChange] = useState('About');
+
+  const renderPage = () => {
+    switch(currentPage) {
+      case "about":
+        return <About></About>
+        case "contact":
+          return <Contact></Contact>
+          case "portfolio":
+            return<Portfolio></Portfolio>
+          case "resume" :
+            return<Resume></Resume>
+           default:
+            return<About></About>  
+    }
+  };
+
+
+  return(
+    <div>
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange}/>
+    <div>
+      {
+        renderPage()
+      }
+      </div>  
+    </div>
+  );
+}
