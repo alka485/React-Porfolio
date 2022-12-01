3eb25e0f43721bcefad3e00369febb01a6575a62
alkasah <alkasah485@gmail.com>
2022-12-01 01:53:51 -0800

    edited about me page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,30 +1,22 @@
 import React from 'react';
+import myImage from '../assets/Portfolio page.jpg';
+import { Col } from 'react-bootstrap';
 
 export default function About() {
   return (
-    // <div style={{backgroundColor : "lightcyan", width : "60rem"}}>
-    //   <h1>About Me</h1>
-    //   <img src= "https://unsplash.com/photos/yScEkBWloPw" alt= "portfolioimage" width="200px"></img>
-      
-    //   <p>
-    //   Hello There ! I am Alka Sah. Having spent of a large chunk of my life  in Banking and customer service . I have finally found my passion
-    //   in web design and technology. I'm an exceptionally creative designer and a coding elf.  I also create,evaluate,and improve product designs.
-    //   truly believe in what I am doing as an user experience evangelist.
-    //  </p>   
-    // </div>
-
-    <section className='container'>
-      <h2 className='top-title'>Alka Sah</h2>
-      <hr></hr>
-      <div>
-        <img class = "mb-5" id='avatar' src='https://photos.google.com/photo/AF1QipNKg8UBPrjDjc7OxARnH5NL43xV8d4LC5PCtEW_' alt='Alka Sah' height= "100px" width= "100px"></img>
-        <p>
-        Hello There ! I am Alka Sah. Having spent of a large chunk of my life  in Banking and customer service . I have finally found my passion
-        in web design and technology. I'm an exceptionally creative designer and a coding elf.  I also create,evaluate,and improve product designs.
+    <section className='title'>
+      <div className="row justify-content-center" id="about-container">       
+      <Col lg={6} md={12}>
+      <center><img src={myImage} className="my-image" alt="Alka Sah" width= "300" height= "300" style={{borderRadius : "50%"}}/></center>
+      <p class = "name">Alka Sah</p>
+      <p class="job">full stack web developer</p>
+      <h2 class="education">a little about me</h2>
+      <p>
+         Hello There ! I am Alka Sah. Having spent of a large chunk of my life  in Banking and customer service . I have finally found my passion
+         in web design and technology. I'm an exceptionally creative designer and a coding elf.  I also create,evaluate,and improve product designs.
          truly believe in what I am doing as an user experience evangelist.  
-        </p>
-                
-                
+      </p>
+      </Col>
       </div>
     </section>
   );
