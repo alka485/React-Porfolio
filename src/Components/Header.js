a19a35361b87c3df5a698f6ada092262ef67365d
alkasah <alkasah485@gmail.com>
2022-12-01 01:06:49 -0800

    updation in App.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Route,Routes, HashRouter } from "react-router-dom";
+import React, { useState } from "react";
+
 import Navigation from "../Components/Navigation";
 import About from "../Components/About";
 import Portfolio from "../Components/Portfolio";
@@ -7,27 +7,3 @@ import Contact from '../Components/Contact';
 import Resume from '../Components/Resume';
 
 
-class Header extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <header>
-          <Navigation />
-        </header>
-
-        <div className="content">
-            
-          <Routes>
-          <Route path="/about" element = {<About/>} />
-          <Route path="/portfolio" element = {<Portfolio/>} />
-          <Route path="/contact" element ={<Contact />}/>
-          <Route path="/resume" element = {<Resume/>}/>
-          </Routes>
-          
-        </div>
-      </HashRouter>
-    );
-  }
-}
-
-export default Header;
